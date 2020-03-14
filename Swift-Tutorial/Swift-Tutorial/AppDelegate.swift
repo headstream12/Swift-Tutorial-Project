c3332504455a4fb9c86096c0d3bbe8f34b1ad8f1
Igor Kryukov <kryukoviv6@gmail.com>
2020-03-14 20:46:14 +0400

    added func "square"

--- Swift-Tutorial/Swift-Tutorial/AppDelegate.swift
+++ Swift-Tutorial/Swift-Tutorial/AppDelegate.swift
@@ -31,6 +31,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
+    
+    private func square(_ number: Int) -> Int {
+        let result = number * number
+        return result
+    }
 
 
 }
