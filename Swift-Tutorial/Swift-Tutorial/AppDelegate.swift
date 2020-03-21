c2eb8e7c0d5f0721d1bf6cc654488f2d3798e4e4
Ilya Glazunov <oxide-wow@ya.ru>
2020-03-21 12:25:54 +0400

    123

--- Swift-Tutorial/Swift-Tutorial/AppDelegate.swift
+++ Swift-Tutorial/Swift-Tutorial/AppDelegate.swift
@@ -18,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     ) -> Bool {
         true
     }
+    
 
     func applicationWillEnterForeground(_ application: UIApplication) {
     }
